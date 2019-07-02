90283067590a56d0f81d1e74ac126d903c027d29
Laura Krayacich <laura@krayacich.com.ar>
2019-07-02 15:04:29 -0300

    old views in separate folder, merge web.php

--- contrareloj/routes/web.php
+++ contrareloj/routes/web.php
@@ -15,18 +15,12 @@ Route::get('/', function () {
     return view('front.index');
 });
 
-/* Route::get('/challenge', function() {
-    return view('front.challenge');
-}); */
-
 Route::get('/profile', function() {
     return view('front.profile');
 });
-<<<<<<< HEAD
 
 Route::get('/challenge', 'ChallengeController@challenge');
-=======
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> 3a6fa9654615c06e0576fda3298f29dc1b3b8857
