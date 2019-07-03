04aeb2e4e3d054895d577bb6f4288df3074b3e1f
virginiarevora <virginiarevora@gmail.com>
2019-07-03 19:27:49 -0300

    otro intento

--- contrareloj/routes/web.php
+++ contrareloj/routes/web.php
@@ -22,11 +22,8 @@ Route::get('/', function () {
 Route::get('/profile', function() {
     return view('front.profile');
 });
-<<<<<<< HEAD
 
 Route::get('/challenge', 'ChallengeController@challenge');
-=======
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> 3a6fa9654615c06e0576fda3298f29dc1b3b8857
