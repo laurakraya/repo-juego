9dfaff6160386d0c10e12d663282622c1a9afd8a
Mbarreneche <mjbarreneche@gmail.com>
2019-07-04 12:00:09 -0300

    guest image y loguin image

--- contrareloj/resources/views/front/profile.blade.php
+++ contrareloj/resources/views/front/profile.blade.php
@@ -10,7 +10,7 @@
     <h1 class="profile__title"></h1>
     <div class="profile__info">
       <div class="profile__info__avatar">
-        <div class="profile__info__avatar__pic" style="background-image: url('/storage/{{Auth::user()->user_image }}')"></div>
+        <div class="profile__info__avatar__pic" style="background-image: url('@guest{{asset('img/profile-placeholder.png')}} @else /storage/{{Auth::user()->user_image }}@endguest')"></div>
       </div>
       <div class="profile__info__data">
         <ul class="profile-data__list">
@@ -31,6 +31,7 @@
     </div>
     <div class="change-avatar">
       <form class="change-avatar__form" action="" method="POST" enctype="multipart/form-data">
+          @csrf
         <label class="change-avatar__form__label" for="avatar">Cambiar avatar:</label>
         <div>
           <input class="change-avatar__form__input" type="file" id="user_image" class="form-control" name="user_image">
