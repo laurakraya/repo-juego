d35112df72bef5d0467c1eeba01eb251909c8c65
virginiarevora <virginiarevora@gmail.com>
2019-07-09 14:02:10 -0300

    me olvide como era el CSS

---
+++ contrareloj/resources/views/front/ranking.blade.php
@@ -0,0 +1,13 @@
+@extends('front.layout')
+
+@section('content')
+<main class="t">
+  <div class="area-info">
+    <section class="ranking">
+      <h1>Ranking</h1>
+    </section>
+    <section class="niveles">
+      <h1>Niveles</h1>
+    </section>
+  </div >
+</main>
