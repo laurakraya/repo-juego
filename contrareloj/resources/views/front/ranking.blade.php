30dd4fdb1ee2ce167091597d4d10a48c47376909
virginiarevora <virginiarevora@gmail.com>
2019-07-09 19:56:06 -0300

    css ranking

--- contrareloj/resources/views/front/ranking.blade.php
+++ contrareloj/resources/views/front/ranking.blade.php
@@ -1,13 +1,20 @@
 @extends('front.layout')
 
 @section('content')
-<main class="t">
+<main class="content">
   <div class="area-info">
     <section class="ranking">
       <h1>Ranking</h1>
+      <div class="table">
+        <div class="table__row">
+          <div class="table__row__pos"> 1</div>
+          <div class="table__row__name">ana</div>
+          <div class="table__row__puntos">1098765432345678900 </div>
+        </div>
+      </div>
     </section>
     <section class="niveles">
-      <h1>Niveles</h1>
+      <h1>Volver a Jugar</h1>
     </section>
   </div >
 </main>
