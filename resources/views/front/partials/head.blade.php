aa50819d1dd94bef769ef0fb9fd11058bd2af9a8
Laura Krayacich <laura@krayacich.com.ar>
2019-07-16 08:48:01 -0300

    browser tab icon

--- resources/views/front/partials/head.blade.php
+++ resources/views/front/partials/head.blade.php
@@ -3,6 +3,7 @@
 <meta http-equiv="ScreenOrientation" content="autoRotate:disabled">
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="icon" type="image/png" href="{{asset('img/logo-reloj.png')}}" sizes="32x32">
 <title>{{ config('app.name', 'Contrareloj') }}</title>
 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
